bcd7280e1b65c26a96dc44ba2384d91e370d46c2
Junita Sinambela <jujunale@gmail.com>
2015-11-23 16:44:56 +0700

    add simpleclient

--- src/main/java/com/mycompany/mongodb/tweet/SimpleClient.java
+++ src/main/java/com/mycompany/mongodb/tweet/SimpleClient.java
@@ -5,6 +5,21 @@
  */
 package com.mycompany.mongodb.tweet;
 
+import java.util.Scanner;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.ServerAddress;
+import org.bson.Document;
+import com.mongodb.client.FindIterable;
+import com.mongodb.Block;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.ascending;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.Arrays.asList;
+import java.util.Date;
+import java.util.Iterator;
+
 
 /**
  *
@@ -15,25 +30,13 @@ public class SimpleClient {
     public MongoDatabase db;
     public String user;
     
-   
-    public Session getSession() {
-        return this.session;
-    }
-    
-    public void connect(String node) {
-        mongoClient = new MongoClient(Arrays.asList(
-                            new ServerAddress("167.205.35.19", 27017),
-                            new ServerAddress("167.205.35.20", 27017),
-                            new ServerAddress("167.205.35.21", 27017)));
-        System.out.printf("Connected to : %s\n");
-//        for ( Host host : metadata.getAllHosts() ) {
-//           System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
-//              host.getDatacenter(), host.getAddress(), host.getRack());
-//        }
+    public void connect(String ip) {
+        mongoClient = new MongoClient(ip, 27017);
         db = mongoClient.getDatabase("13512023");
     }
     
     public void close() {
+        mongoClient.close();
     }
     
     public boolean handleCommand(String command) {
@@ -45,11 +48,18 @@ public class SimpleClient {
             if(s.length == 2){
                 String username = split[1].split(" ")[0];
                 String password = split[1].split(" ")[1];
-                session.execute(
-                        "INSERT INTO users (username, password) VALUES ('"+
-                                username +"','"+password+"');"
-                );
-                System.out.println(username + " successfully registered");
+                FindIterable<Document> iterable = db.getCollection("users").find(eq("username", username));
+                Document doc = iterable.first();
+                if(doc == null) {
+                    db.getCollection("users").insertOne(
+                        new Document()
+                        .append("username", username)
+                        .append("password", password)
+                    );
+                    System.out.println(username + " successfully registered");
+                } else {
+                    System.out.println("Username already exist");
+                }
             }
             else{
                 System.out.println("Usage : register <username> <password>");
@@ -59,9 +69,9 @@ public class SimpleClient {
             if(s.length == 2){
                 String username = split[1].split(" ")[0];
                 String password = split[1].split(" ")[1];
-                ResultSet results  = session.execute(
-                        "SELECT username, password FROM users WHERE username = '" + username+"';");
-                if(results.one().getString("password").equals(password)) {
+                FindIterable<Document> iterable = db.getCollection("users").find(and(eq("username", username), eq("password", password)));
+                Document doc = iterable.first();
+                if(doc != null) {
                     user = username;
                     System.out.println("Hello, " + user + " !");
                 } else {
@@ -75,89 +85,90 @@ public class SimpleClient {
             if (user != null) { 
                 String tofollow = split[1];
                 
-                session.execute(
-                        "INSERT INTO friends (username, friend, since) VALUES ('"+
-                                tofollow + "','" + user + "',dateof(now()));"
-                );
-                session.execute(
-                        "INSERT INTO followers (username, follower, since) VALUES ('"+
-                                tofollow + "','" + user + "',dateof(now()));"
-                );
-                System.out.println("Successfully follow " + tofollow);
+                FindIterable<Document> iterable = db.getCollection("users").find(eq("username", tofollow));
+                Document doc = iterable.first();
+                if(doc != null) {
+                    db.getCollection("users").updateOne(new Document("username", user),
+                                new Document("$addToSet", new Document("friends", tofollow)));
+                    db.getCollection("users").updateOne(new Document("username", tofollow),
+                                new Document("$addToSet", new Document("followers", user)));
+                    System.out.println("Successfully follow " + tofollow);
+                } else {
+                    System.out.println("No user with username " + tofollow);
+                }
             } else {
                 System.out.println("Please login first");
             }
         } else if (split[0].equals("tweet") && split.length == 2) {
             if (user != null) {
-                String tweetBody = split[1];
-                UUID timeuuid = UUIDs.timeBased();
-                UUID tweetuuid = UUIDs.timeBased();
-                session.execute(
-                        "INSERT INTO tweets (tweet_id, username, body) VALUES ("+
-                                tweetuuid + ",'" + user + "','" + tweetBody +"');"
+                final String tweetBody = split[1];
+                final Date date = new Date();
+                db.getCollection("users").updateOne(new Document("username", user),
+                        new Document("$push", new Document("tweets",
+                            new Document()
+                                .append("body", tweetBody)
+                                .append("timestamp", date.toString()))
+                        )
                 );
-                session.execute(
-                        "INSERT INTO userline (username, time, tweet_id) VALUES ('"+
-                                user + "'," + timeuuid + "," + tweetuuid +");"
+                db.getCollection("users").updateOne(new Document("username", user),
+                        new Document("$push", new Document("timeline",
+                            new Document("username", user)
+                                .append("body", tweetBody)
+                                .append("timestamp", date.toString()))
+                        )
                 );
-                session.execute(
-                        "INSERT INTO timeline (username, time, tweet_id) VALUES ('"+
-                                user + "'," + timeuuid + "," + tweetuuid +");"
-                );
-                List<String> followers = new ArrayList<>();
-                ResultSet results  = session.execute(
-                    "SELECT username, follower FROM followers WHERE username = '"
-                            + user+"';");
-                for(Row row : results) {
-                    followers.add(row.getString("follower"));
-                }
-                for (String follower : followers) {
-                    session.execute(
-                            "INSERT INTO timeline (username, time, tweet_id) VALUES ('"+
-                                    follower +"'," + timeuuid + "," + tweetuuid+");"
-                    );
-                }
+                FindIterable<Document> iterables = db.getCollection("users").find(eq("username", user));
+                iterables.forEach(new Block<Document>() {
+                    @Override
+                    public void apply(final Document document) {
+                        final ArrayList<String> followers = (ArrayList<String>) document.get("follows");
+                        if(followers != null)
+                            for(String follower: followers){
+                                db.getCollection("users").updateOne(new Document("username", follower),
+                                new Document("$push", new Document("timeline",
+                                            new Document("username", user)
+                                                .append("body", tweetBody)
+                                                .append("timestamp", date.toString()))
+                                        )
+                                );
+                            }
+                    }
+                });
             } else {
                 System.out.println("Please login first");
             }
         } else if (split[0].equals("showuserline") && split.length == 2) {
             if (user != null) {
-                String toView = split[1];
-                ResultSet tweetid = session.execute(
-                        "SELECT time, tweet_id FROM userline WHERE username ='"+
-                                toView+"'ORDER BY time DESC;");
-                List<UUID> tweetids = new ArrayList<>();
-                for (Row row : tweetid) {
-                    tweetids.add(row.getUUID("tweet_id"));
-                }
-                for (UUID id : tweetids) {
-                    ResultSet result = session.execute(
-                            "SELECT username, body FROM tweets WHERE tweet_id ="+
-                                    id+";");
-                    Row row = result.one();
-                    System.out.println(row.getString("username")+" : "+
-                            row.getString("body"));
-                }
+                final String toView = split[1];
+                FindIterable<Document> iterable = db.getCollection("users").find(eq("username", toView));
+                iterable.forEach(new Block<Document>() {
+                    @Override
+                    public void apply(final Document document) {
+                        final ArrayList<Document> documents = (ArrayList<Document>) document.get("tweets");
+                        if(documents != null)
+                            for(int i = documents.size() - 1; i >= 0; i--){
+                                Document d = documents.get(i);
+                                System.out.println( toView +" : "+ d.getString("body"));
+                            }
+                    }
+                });
             } else {
                 System.out.println("Please login first");
             }
         } else if (split[0].equals("showtimeline")) {
             if (user != null) {
-                ResultSet tweetid = session.execute(
-                        "SELECT time, tweet_id FROM timeline WHERE username ='"+
-                                user+"'ORDER BY time DESC;");
-                List<UUID> tweetids = new ArrayList<>();
-                for (Row row : tweetid) {
-                    tweetids.add(row.getUUID("tweet_id"));
-                }
-                for (UUID id : tweetids) {
-                    ResultSet result = session.execute(
-                            "SELECT username, body FROM tweets WHERE tweet_id ="+
-                                    id+";");
-                    Row row = result.one();
-                    System.out.println(row.getString("username")+" : "+
-                            row.getString("body"));
-                }
+                FindIterable<Document> iterable = db.getCollection("users").find();
+                iterable.forEach(new Block<Document>() {
+                    @Override
+                    public void apply(final Document document) {
+                        final ArrayList<Document> documents = (ArrayList<Document>) document.get("timeline");
+                        if(documents != null)
+                            for(int i = documents.size() - 1; i >= 0; i--){
+                                Document d = documents.get(i);
+                                System.out.println( d.getString("username") +" : "+ d.getString("body"));
+                            }
+                    }
+                });
             } else {
                 System.out.println("Please login first");
             }
